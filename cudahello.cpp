3daf118ed549c3eb44df4643d863fc3c845d6eab
rgayatri <rgayatri@lbl.gov>
2021-02-18 10:29:50 -0800

    Adding the hello-world test case that prints the number of threads/GPUs attached to each rank.

---
+++ cudahello.cpp
@@ -0,0 +1,70 @@
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <mpi.h>
+#include <omp.h>
+#include <sched.h>
+#include <stdio.h>
+
+int find_gpus(void) {
+    int ngpu;
+    cudaGetDeviceCount(&ngpu);
+    return ngpu;
+}
+void gpu_pci_id(char* device_id, int device_num) {
+    int len;
+    cudaDeviceGetPCIBusId(device_id, len, device_num);
+    return;
+}
+
+extern int sched_getcpu(void);
+
+int main(int argc, char* argv[]) {
+    int myid, namelen, world_size;
+    char myname[MPI_MAX_PROCESSOR_NAME];
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+    MPI_Get_processor_name(myname, &namelen);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    int ngpu = find_gpus();
+    char my_gpu[15];
+
+    fprintf(stdout,
+            "Hello Rahul from processor %s, rank = %d out of %d processors"
+            "\n",
+            myname, myid, world_size);
+    char my_gpu_id[15];
+    gpu_pci_id(my_gpu_id, myid);
+    fprintf(stdout, "My GPU = %s\n", my_gpu_id);
+
+    fprintf(stdout, "I see a total of %d GPUs and the other PCI IDs are: \n",
+            ngpu);
+
+    for (int j = 0; j < ngpu; j++) {
+        if (j != myid) {
+            char gpu_id[15];
+            gpu_pci_id(gpu_id, j);
+            fprintf(stdout, "rank = %d: %s\n", j, gpu_id);
+        }
+    }
+
+    // synchronize so the loop guarantees to prints all the information of one
+    // rank before progressing.
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    fprintf(stdout, "CPUs and Threads assigned to me are \n");
+
+#pragma omp parallel for ordered
+    for (int i = 0; i < omp_get_max_threads(); ++i) {
+#pragma omp ordered
+        fprintf(stdout, "rank = %04d, thread = %03d, cpu = %03d\n", myid,
+                omp_get_thread_num(), sched_getcpu());
+    }
+
+    fprintf(stdout,
+            "\n****************************************************************"
+            "******************** \n");
+
+    MPI_Finalize();
+}
